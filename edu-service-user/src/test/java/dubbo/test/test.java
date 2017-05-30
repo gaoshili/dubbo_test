a80f94f3174552fa858c1332b41e765a4126f7c5
gaoshili <987766192@qq.com>
2017-05-30 14:51:33 +0800

    dubbo_test：dubbo测试项目

---
+++ edu-service-user/src/test/java/dubbo/test/test.java
@@ -0,0 +1,13 @@
+package dubbo.test;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * Created by gaoshili on 2017-05-30.
+ */
+public class test {
+
+    public static void main(String[] args){
+       System.out.print( DigestUtils.sha1Hex("wusc.123"));
+    }
+}
